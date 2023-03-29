6fdcb044561680440ed4dbc0015a7cec75f8ed94
MohitChristian44 <mohitchristian4444@gmail.com>
2023-03-29 19:07:22 -0400

    The hello world program in Bash

---
+++ MK1.sh
@@ -0,0 +1,9 @@
+# to make a Bash file we write the following into terminal
+# touch <file_name>.sh
+#! /bin/bash 
+
+echo "This is my first program"
+# the echo command prints everything onto the screen
+# to make this file executable we write 
+# chmod +x <file_name>.sh
+#To run the file write ./<file_name>.sh
